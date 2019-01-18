def1704625126d638012b916fb682dcce6dcf19b
Nathan Nielson <nathanbn123@gmail.com>
2019-01-18 10:46:39 -0800

    it will create a if statement but won't work

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-$(function(event){
-  $(".btn").click(function(){
+$(function(){
+  $(".btn").click(function(event){
   var intInput =  $("#interest").val();
   var prefInput = $("#prefrence").val();
   var mobInput = $("#mobile").val();
 
 
-  if(intInput === "big" || "both")
+  if(intInput === "big" || "both" && prefInput === "big" && mobInput === "yes") {
+    $(".suggestion .java").toggleFade();
+  }
+
+event.preventDefault();
 
 
 
 
 
-
-
+  });
 });
