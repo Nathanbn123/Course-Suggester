079fde5043dc738b036d7d54490ce5c2ffd0634e
Nathan Nielson <nathanbn123@gmail.com>
2019-01-18 15:00:51 -0800

    it will add logo and styling while trying to center output

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,29 @@
 $(function(){
-
   $(".btn").click(function(event){
-
   var intInput = $("#interest").val();
   var prefInput = $("#prefrence").val();
   var mobInput = $("#mobile").val();
-
-
-
-$('.col-lg-4').fadeOut();
+  
+  $('.col-lg-4').fadeOut();
 
   if (mobInput === "yes"){
-
     $("#java").fadeIn();
   }
 
 
   if (intInput === "design") {
     if (prefInput === "both" || "small" && (mobInput === "no")){
-
       $("#css").fadeIn();
   }
 }
 
-if (intInput === "function") {
-  if (mobInput === "yes"){
-    $("#ruby").fadeIn();
-}
+  if (intInput === "function" || "both2") {
+    if (mobInput === "yes"){
+      $("#ruby").fadeIn();
+  }
 }
 
 event.preventDefault();
-
-
-
-
-
 
   });
 });
