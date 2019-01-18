c9749bb0e282c6dfd300b44c4f77d9f7f3b7fafa
Nathan Nielson <nathanbn123@gmail.com>
2019-01-18 10:22:20 -0800

    it will add inputs that I also totally forgot about

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-function courseType ()
+function courseSuggest(input1, input2, input3) {
+  if(input1 === "big") 
+
+}
+
+
+
+
+
+
+
+
+$(function(event){
+  $(".btn").click(function(){
+
+
+
+
+
+
+
+
+});
