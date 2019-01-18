c8f0f00a4fd7f648d5e9f9009134b8bfc7709c5e
Nathan Nielson <nathanbn123@gmail.com>
2019-01-18 13:00:23 -0800

    it will finally actually work after much time wasted when I could have just done this

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
 $(function(){
-  $(".btn").click(function(){
-  var intInput =  $("#interest").val();
+  $(".btn").click(function(event){
+  var intInput = $("#interest").val();
   var prefInput = $("#prefrence").val();
   var mobInput = $("#mobile").val();
 
 
 
 
-  if(intInput === "function" || "both") {
-    $(".java").fadeToggle();
+  if(intInput === "function") {
+    if (prefInput === "big" && (mobInput === "yes")){
+      $(".java").fadeToggle();
+    }
+}
+
+    if (intInput === "design") {
+  if (prefInput === "both" && (mobInput === "yes")){
+    $(".css").fadeToggle();
+    }
   }
 
+  if (intInput === "design") {
+if (prefInput === "small" && (mobInput === "yes")){
+  $(".ruby").fadeToggle();
+  }
+}
 
-
+event.preventDefault();
 
 
 
