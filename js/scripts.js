1102b651ca696831a05a546ce980341c359bd937
Nathan Nielson <nathanbn123@gmail.com>
2019-01-18 11:33:24 -0800

    it will finally work after fixing button and fadeTOGGLE method

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(function(){
-  $(".btn").click(function(event){
+  $(".btn").click(function(){
   var intInput =  $("#interest").val();
   var prefInput = $("#prefrence").val();
   var mobInput = $("#mobile").val();
 
 
-  if(intInput === "big" || "both" && prefInput === "big" && mobInput === "yes") {
-    $(".suggestion .java").toggleFade();
+
+
+  if(intInput === "function" || "both") {
+    $(".java").fadeToggle();
   }
 
-event.preventDefault();
+
+
+
 
 
 
